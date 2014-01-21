61985ef399a1f5ca96bbc361a44a64d89b485711
David Persson <davidpersson@gmx.de>
2014-01-21 16:43:50 +0100

    Use panes.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -10,13 +10,17 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
 
-use cms_core\extensions\cms\Modules;
+use cms_core\extensions\cms\Panes;
 use lithium\g11n\Message;
 use cms_media\models\Media;
 
 extract(Message::aliases());
 
-Modules::register('cms_agency_portfolio', 'projects', ['title' => $t('Portfolio Projects')]);
+Panes::register('cms_agency_portfolio', 'projects', [
+	'title' => $t('Portfolio Projects'),
+	'group' => Panes::GROUP_AUTHORING,
+	'url' => ['controller' => 'projects', 'library' => 'cms_agency_portfolio', 'admin' => true]
+]);
 Media::registerDependent('cms_agency_portfolio\models\Projects', [
 	'cover' => 'direct', 'media' => 'joined'
 ]);
