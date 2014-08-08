557d0334d0309d76a745985d9e9f6537439a8c28
David Persson <davidpersson@gmx.de>
2014-08-08 15:01:55 +0200

    Correct schema.

--- data/schema.sql
+++ data/schema.sql
@@ -1,11 +1,19 @@
-CREATE TABLE `posts` (
+CREATE TABLE `portfolio_projects` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `cover_media_id` int(11) NOT NULL,
+  `cover_media_id` int(11) unsigned NOT NULL,
+  `order` int(11) unsigned DEFAULT NULL,
   `title` varchar(250) NOT NULL DEFAULT '',
+  `client` varchar(250) DEFAULT NULL,
+  `urls` mediumtext,
   `teaser` text,
   `body` text NOT NULL,
-  `tags` varchar(250) DEFAULT NULL,
+  `is_promoted` tinyint(1) DEFAULT '0',
+  `is_published` tinyint(1) DEFAULT '0',
+  `published` date DEFAULT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `is_published` (`is_published`),
+  KEY `is_promoted` (`is_promoted`),
+  KEY `order` (`order`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
