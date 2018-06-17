09b0edbd5f9abfc048b9580bba2189b7ae3b5c6e
Marius Wilms <mariuswilms@mailbox.org>
2018-06-17 19:37:23 +0200

    Inherit default charset and collation

--- data/upgrade/001-single-url.sql
+++ data/upgrade/001-single-url.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `portfolio_projects` CHANGE `urls` `url` VARCHAR(250)  CHARACTER SET utf8  COLLATE utf8_general_ci  NULL  DEFAULT NULL;
+ALTER TABLE `portfolio_projects` CHANGE `urls` `url` VARCHAR(250)  NULL  DEFAULT NULL;
